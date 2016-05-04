25b9b8713638cbe39605acb0b23335cd965835a5
Ryan Kim <Ryanfsdf@v1020-wn-89-127.campus-dynamic.uwaterloo.ca>
2016-05-04 15:02:18 -0400

    Changed Window Size and Took Out Resizing

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -21,7 +21,8 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("JavRuler");
         primaryStage.setAlwaysOnTop(true);
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 250, 100));
         primaryStage.show();
         primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             public void handle(WindowEvent we) {
